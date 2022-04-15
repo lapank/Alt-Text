2fe4c345d074726f2bd36d154d6ab98c1e1cdf39
Kaylin Lapan <kaylinlapan@gmail.com>
2022-04-15 15:03:34 -0400

    Added punctuation check.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
 const maxChar = 160;
 const count = document.getElementById('count');
+const punct = document.getElementById('punct');
+const punctuation =[' ',',','.',';',':','-','!','"',"'",'(',')'];
 
 let charCount = e => {
   const keyName = e.key;
   let altText = document.getElementById('altText').value.length;
   countColor(altText);
   count.innerHTML = altText;
+  if (punctCheck(keyName)){
+    punct.innerHTML = "Yes";
+  }
   
 }
 
@@ -22,5 +27,16 @@ let countColor = (altText) =>{
       count.style.color = 'green';
   }
 } 
+
+let punctCheck = keyName => {
+  let found = false;
+  for(let i= 0; i < punctuation.length; i++){
+    if(punctuation[i] == keyName){
+      found = true;
+    }
+  }
+  return (found);
+  
+}
 
 document.addEventListener('keyup', charCount);
