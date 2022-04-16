0737a5fdb290bd38d46a428b47213353e6ec5b79
Kaylin Lapan <kaylinlapan@gmail.com>
2022-04-15 22:13:39 -0400

    Adding more words and types of language to include in alt text.

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 
-const maxChar = 160;
+const maxChar = 150;
 const count = document.getElementById('count');
 const punct = document.getElementById('punct');
 const color = document.getElementById('color');
 const punctuation =[',','.',';',':','-','!','"',"'",'(',')','?'];
-const colorList = ['aqua','beige','black','blue','brown','dark blue', 'dark gray', 
-              'dark green', 'dark purple', 'dark red', 'dark orange', 'gold',
-              'gray', 'green', 'light gray', 'light green', 'light blue', 
-              'maroon', 'orange', 'pink', 'red','white','yellow']
+const colorList = ['red', 'orange', 'yellow', 'green', 'blue','purple', 
+                  'pink', 'brown', 'gold', 'bronze', 'silver', 'black', 'white'];
+const planeParts = ['wing', 'body', 'tail', 'wheels', 'engine', 'nose', 'cockpit'];
+const shapes = ['circle', 'triangle', 'rectangle', 'oval', 'square', 'pentagon', 
+                  'hexagon', 'octagon', 'cube', 'cone', 'cylinder', 'sphere'];
+const pronouns = ['he', 'him', 'his', 'she', 'her', 'hers', 'man', 'woman', 'lady', 'gentilmen', 'guy', 'gal', 'man', 'woman'];
+const emotions = ['happy', 'sad', 'angrey', 'solumn', 'serious'];
+const thirdPerson = ['it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves'];
+const positions = ['left', 'right', 'top', 'bottom', 'around', 'under', 'below', 
+                  'behind', 'beside', 'between', 'after', 'across', 'inside', 
+                  'near', 'on', 'outside', 'forward', 'backward', 'side', 'top'];
+const size = ['big', 'small', 'tall', 'wide', 'short', 'large', 'heavy', 
+            'light', 'thin', 'thick', 'long', 'narrow', 'meters', 'feet', 'inches', 'centemeters'];
+const medium = ['radiograph', '3D model', 'drawing, painting', '35mm slide', 'print', 'scientific illustration'];
+const condition = ['rips', 'tears', 'flaking', 'marks', 'weathered', 'shiny'];
+const age = ['young adult', 'adult', 'child', 'infant'];
+const noNoWords = ['elderly', 'image of', 'picture of', ' I ', "we", 'our', 'my', 'me'];
+
 let found = false;
 //let punctCount = 0;
 
@@ -17,7 +31,7 @@ let charCount = e => {
   let altChar = document.getElementById('altText').value;
   punctCheck(altChar);
   countColor(altCount);
-  count.innerHTML = altCount;
+  count.innerHTML = altCount + "/" + maxChar;
   if(colorCheck(altChar)){
     color.innerHTML = "Yes";
   }else{
@@ -32,10 +46,10 @@ let charCount = e => {
 
 let countColor = (altCount) =>{
   switch (true) {
-    case (altCount > 100 && altCount <= 140):
+    case (altCount > 100 && altCount <= 125):
       count.style.color = 'gold';
       break;
-    case (altCount > 140 && altCount <= maxChar):
+    case (altCount > 125 && altCount <= maxChar):
       count.style.color = 'red';
       break;
     default:
@@ -74,3 +88,25 @@ document.addEventListener('keyup', charCount);
       ---ooO-(_)-Ooo---
       Kaylin was here.
 */
+
+//do not repeat text accompanying image
+//use third person unless using embodiment technique with 'you' to instruct user to do somethign or imagine something.
+//Subject, action, context.  A _____ does something from somewhere.
+//Use plain speech.
+//Do not use "This is an image/picture/photograph of...""
+//transcribe text that appears in image surrounded by quotes.  Illegible portions in brackets []
+//Use metric system for measurments
+//relate size of elements in image to other elements of image or real life items.
+//condition of specimen
+//use name of individual is smithsonian staff, person of note, or celebrity, do not identify anyone else.
+//say race if varified, do not assume.
+//describe if someone is in a wheel chair or using crutches.
+//use metaphors "shaped like a___"
+//if the caption or description pared with the image describes the image perfectly, don't include alt text.
+
+
+
+
+
+
+
