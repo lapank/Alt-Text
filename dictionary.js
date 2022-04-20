499eaf80fdc6f11cb89b0c20736a934345dc2be0
Kaylin Lapan <kaylinlapan@gmail.com>
2022-04-20 00:34:11 -0400

    Seperating word lists from functionality.  Adding styles.

---
+++ dictionary.js
@@ -0,0 +1,30 @@
+// Alt text should have:
+const punctuation =['.','!','?'];
+const thirdPerson = ['it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves'];
+
+// Alt text recommendations:
+const colorList = ['red', 'orange', 'yellow', 'green', 'blue','purple', 
+                  'pink', 'brown', 'gold', 'bronze', 'silver', 'black', 'white'];
+const planeParts = ['wing', 'body', 'tail', 'wheels', 'engine', 'nose', 'flightdeck'];
+const shapes = ['circle', 'triangle', 'rectangle', 'oval', 'square', 'pentagon', 
+                  'hexagon', 'octagon', 'cube', 'cone', 'cylinder', 'sphere'];
+const emotions = ['happy', 'sad', 'angrey', 'solumn', 'serious'];
+const positions = ['left', 'right', 'top', 'bottom', 'around', 'under', 'below', 
+                  'behind', 'beside', 'between', 'after', 'across', 'inside', 
+                  'near', 'on', 'outside', 'forward', 'backward', 'side', 'top'];
+const size = ['big', 'small', 'tall', 'wide', 'short', 'large', 'heavy', 
+            'light', 'thin', 'thick', 'long', 'narrow', 'meters', 'feet', 'inches', 'centemeters'];
+const medium = ['radiograph', '3D model', 'drawing, painting', '35mm slide', 'print', 'scientific illustration'];
+const condition = ['rips', 'tears', 'flaking', 'marks', 'weathered', 'shiny'];
+const age = ['young adult', 'adult', 'child', 'infant'];
+
+// Alt text might have:
+const pronouns = [' he ', ' him ', ' his ', ' she ', ' her ', ' hers ', ' man ', ' woman ', ' lady ', ' gentlemen ', 'guy', 'gal', 'man', 'woman'];
+const firstPerson = ['I ',' me ','Me ',' my ','My ',' mine ', 'Mine ',' we ', 'We ' ,' us ', 'Us ',' our ', 'Our ',' ours ', 'Ours '];
+const secondPerson = ['You ','yall','ya\'ll',' you ','Your ',' your ','Yours ',' yours ','You\'re ',' you\'re '];
+
+
+// Alt text should not have:
+const noNoWords = ['elderly', 'image of', 'picture of', ' I ', "we", 'our', 'my', 'me']; /* poster text*/
+const tenseList = ['ing', 'been', 'will', 'was', 'had'];
+
